959bdfb6ea3f7f0e9acc680e5aa382e1a5b3a993
AlfonsoGolino <46001435+AlfonsoGolino@users.noreply.github.com>
2020-09-06 18:22:13 +0200

    Inserimento Automatico

--- DownloadFile.js
+++ DownloadFile.js
@@ -1,17 +1,18 @@
 
-fs = require('fs');
-var request = require('request');
-var url = 'https://raw.github.com/pcm-dpc/COVID-19/master/legacy/dati-regioni/dpc-covid19-ita-regioni-';
-let date_ob = new Date();
-var data = date_ob.getFullYear() + '' + ("0" + (date_ob.getMonth() + 1)).slice(-2) + '' + ("0" + date_ob.getDate()).slice(-2);
-url = url + '' + data+'.csv';
-request.get(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var csv = body;
-        var file = 'dpc-covid19-ita-regioni-' + data+'.csv';
-        fs.writeFile(file, body, function (err) {
-            if (err) return console.log(err);
-            console.log('done');
-        });
-    }
+        fs = require('fs');
+        var request = require('request');
+        var url = 'https://raw.github.com/pcm-dpc/COVID-19/master/legacy/dati-regioni/dpc-covid19-ita-regioni-';
+        let date_ob = new Date();
+        var data = date_ob.getFullYear() + '' + ("0" + (date_ob.getMonth() + 1)).slice(-2) + '' + ("0" + date_ob.getDate()).slice(-2);
+        url = url + '' + data + '.csv';
+        request.get(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var csv = body;
+                var file = './Dataset_Covid-19/dpc-covid19-ita-regioni-' + data + '.csv';
+                fs.writeFile(file, body, function (err) {
+                    if (err) return console.log(err);
+                    console.log('done');
+                    require('./InsertDatiRegione.js');
+                });
+            }
 });
---
+++ InsertDatiRegione.js
@@ -0,0 +1,42 @@
+
+fs = require('fs');
+const mongodb = require("mongodb").MongoClient;
+const csvtojson = require("csvtojson");
+
+fs.readdir('./Dataset_Covid-19', (err, files) => {
+    if (!files.length) {
+        console.log("empty");
+    } else {
+
+        var filePath = './Dataset_Covid-19/' + files[0]
+        let url = "mongodb://localhost:27017/";
+        
+        csvtojson()
+            .fromFile(filePath)
+            .then(csvData => {
+     
+                mongodb.connect(
+                    url,
+                    { useNewUrlParser: true, useUnifiedTopology: true },
+                    (err, client) => {
+                        if (err) throw err;
+
+                        client
+                            .db("Covid-19")
+                            .collection("Regione")
+                            .insertMany(csvData, (err, res) => {
+                                if (err) throw err;
+
+                                console.log(`Inserted: ${res.insertedCount} rows`);
+                                client.close();
+                                //eliminzaione file
+                                fs.unlink(filePath, function (err) {
+                                    if (err) throw err;
+                                    console.log('File deleted!');
+                                });
+                            });
+                    }
+                );
+            });
+    }
+});
